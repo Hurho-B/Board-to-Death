4f6610ba80092636a760a87c0a11fb27e8e41ca4
Brian Mosquera <149270404+bmucf@users.noreply.github.com>
2025-09-22 16:59:43 -0400

    experimenting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,44 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveForce = 50;
+    public float maxSpeed = 10f;
+    public float turnSpeed = 5f;
+
+    public float groundCheckDistance = 0.25f;
+
+    bool isGrounded = false;
     bool isMoving = false;
 
-    public int kickoffPower = 5;
-
-    Vector3 kickOff = new Vector3(0, 5, 5);
-
-    Rigidbody rb;
-
+    Rigidbody skateBoardRB;
 
     void Start()
     {
-        //You get the Rigidbody component you attach to the GameObject
-        rb = GetComponent<Rigidbody>();
 
-        //Initialising the force which is used on GameObject in various ways
-        m_NewForce = new Vector3(-5.0f, 1.0f, 0.0f);
-
-        //Initialising floats
-        m_ForceX = 0;
-        m_ForceY = 0;
-
-        //The forces typed in from the text fields (the ones you can manipulate in Game view)
-        m_ForceXString = "0";
-        m_ForceYString = "0";
-
-        //The GameObject's starting position and Rigidbody position
-        m_StartPos = transform.position;
-        m_StartForce = m_Rigidbody.transform.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
 
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            m_Rigidbody.AddForce(kickOff, ForceMode.Impulse);
-            Debug.Log("space key was pressed");
-        }
     }
 }
